dd1da26f19acec42e8fe160959206d668030ad89
imquak <patinhofeiox@gmail.com>
2023-10-18 20:42:07 +0100

    Added Color Picking and Border
    -You can now change color of pencil
    -You can toggle the border on and off
    -Started work on changing the size of the board

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
 const box = document.getElementById("box");
 console.log(box);
 
-let n = 16;
-box.style.width,box.style.height = n * 16;
+
+
+let n = 64;
+box.style.width,box.style.height = (n * n * 1.5+"px");
 
 let currentColor = "Rgb(0,0,0)";
-
+let isBorder = true;
 let isMouseDown = false;
 let isMouseHover = false;
 
+function changeColor(color){
+    console.log("changed color to "+color);
+    currentColor = color;
+}
+function toggleBorder(){
+    console.log(isBorder);
+    let squares = document.getElementsByClassName("square");
+    Array.from(squares).forEach(element => {
+        if (isBorder){
+            element.style.borderStyle = 'none';
+        }
+        else{
+            element.style.borderStyle = 'solid';
+        }
+    });
+    isBorder = !isBorder;
+}
+
 document.body.onmousedown = function() { 
-    console.log("MOUSE DOWN");
     isMouseDown = true;
 }
 document.body.onmouseup = function() {
-    console.log("MOUSE UP");
     isMouseDown = false;
 }
 function addListeners(obj){
+    let oldColor = obj.style.backgroundColor;
     obj.addEventListener("click", function (event) {
-        obj.style.backgroundColor = currentColor;  
+        obj.style.backgroundColor = currentColor;
+        oldColor = obj.style.backgroundColor;
       }, false);
     obj.addEventListener("mouseleave", function (event) {
         isMouseHover = false
         if (isMouseDown === true){
             obj.style.backgroundColor = currentColor;
+            oldColor = obj.style.backgroundColor;
+        }
+        else{
+            obj.style.backgroundColor = oldColor;
         }
       }, false);
-      
     obj.addEventListener("mouseover", function (event) {
+        obj.style.backgroundColor = currentColor;
         isMouseHover = true
         if (isMouseDown === true){
             obj.style.backgroundColor = currentColor;
+            oldColor = obj.style.backgroundColor;
         }
       }, false);
 }
@@ -41,12 +66,16 @@ function createTiles(n){
         row.className = "row";
         box.appendChild(row);
         for (let i = 0; i < n; i++){
-            const square = document.createElement("div");
+            let square = document.createElement("div");
             square.className = "square";
             addListeners(square);
             row.appendChild(square);
         }
     }
+    let squares = document.getElementsByClassName("square");
+    Array.from(squares).forEach( (element) => {
+        element.style.height = (n / 1.5+"px");
+        element.style.width = (n /1.5+"px");
+    } )
 }
-
 createTiles(n);
